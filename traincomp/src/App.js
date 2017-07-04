ff8c37ead245aece91e6a7bf88d1088e87396e6d
arafatnsu <arafatnsu@yahoo.com>
2017-07-04 22:46:28 +0300

    2nd task

--- traincomp/src/App.js
+++ traincomp/src/App.js
@@ -1,14 +1,47 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import $ from 'jquery'
+import './App.css'
+import Train from './components/train'
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      trains: {}
+    }
+  }
+
+  getTrains() {
+    $.ajax({
+      url:'https://rata.digitraffic.fi/api/v1/live-trains?station=SLO',
+      type: 'GET',
+      success:function(data) {
+        this.setState({trains: data})
+      }.bind(this),
+    })
+}
+
+componentDidMount() {
+  this.getTrains()
+}
+
+
   render() {
+    const trains = this.state.trains
     return (
       <div className="App">
-        Train
+        {trains &&
+          Object.keys(trains).map(key => (
+            <Train
+              key={key}
+              trainType={trains[key].trainType}
+              trainNumber={trains[key].trainNumber}
+            />
+          ))
+        }
       </div>
     );
   }
 }
 
-export default App;
+export default App
